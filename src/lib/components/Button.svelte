d58070205b5564f6da19b3a76322c3f43bfcde5e
n3-rd <godwinjdayo@gmail.com>
2023-04-03 01:06:19 +0100

    Added Button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let content = '';
+	export let radius = '0';
+	export let color = 'blue';
+	export let textColor = 'white';
+	export let rounded = false;
+	export let size = 'md';
+	export let manualSize = null;
+
+	// Define an object to map the predefined size options to their corresponding pixel values
+	const sizeMap = {
+		xs: '10px',
+		sm: '20px',
+		md: '30px',
+		lg: '40px',
+		xl: '50px'
+	};
+</script>
+
+<button
+	style="
+		background-color: {color};
+        color: {textColor};
+		border-radius: {rounded ? '50%' : radius + 'px'};
+        font-size: {manualSize ? manualSize + 'px' : sizeMap[size]};
+	"
+>
+	{content}
+</button>
+
+<style>
+	button {
+		/* make aesthetic and fancy */
+		border: none;
+		padding: 10px;
+		cursor: pointer;
+		transition: filter 0.2s;
+	}
+	button:hover {
+		filter: brightness(1.3);
+	}
+</style>
