2ee6f768c3a3d90ce0723a58dd9eb03399defdfd
n3-rd <godwinjdayo@gmail.com>
2023-04-04 13:18:28 +0100

    added DiamondShape and HeartShape

---
+++ src/lib/components/shapes/DiamondShape.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let size = 50;
+	export let topColor = '#333';
+	export let bottomColor = '#333';
+</script>
+
+<div
+	class="diamond"
+	style="
+    --size:{size}px;
+    --topColor:{topColor};
+    --bottomColor:{bottomColor}
+"
+/>
+
+<style>
+	.diamond {
+		/* Modify size here: */
+		--size: 50px;
+		--quarter-size: calc(var(--size) / 4);
+		--topColor: #333;
+		--bottomColor: #333;
+
+		border-style: solid;
+		border-color: transparent transparent var(--topColor) transparent;
+		border-width: 0 var(--quarter-size) var(--quarter-size) var(--quarter-size);
+		height: 0;
+		width: calc(var(--size) / 2);
+		box-sizing: content-box;
+		position: relative;
+		margin: 0 0 calc(var(--size) / 2) 0;
+	}
+	.diamond:after {
+		content: '';
+		position: absolute;
+		top: var(--quarter-size);
+		left: calc(var(--quarter-size) * -1);
+		width: 0;
+		height: 0;
+		border-style: solid;
+		border-color: var(--bottomColor) transparent transparent transparent;
+		border-width: calc(var(--size) * 0.7) calc(var(--size) / 2) 0 calc(var(--size) / 2);
+	}
+</style>
---
+++ src/lib/components/shapes/HeartShape.svelte
@@ -0,0 +1,44 @@
+<script>
+	export let size = 50;
+	export let color = '#333';
+</script>
+
+<div
+	class="heart"
+	style="
+    --size:{size}px;
+    --color:{color};
+"
+/>
+
+<style>
+	.heart {
+		/* Modify size here: */
+		--size: 50px;
+		--color: #333;
+
+		position: relative;
+		width: var(--size);
+		height: calc(var(--size) * 0.3);
+	}
+
+	.heart:before,
+	.heart:after {
+		position: absolute;
+		content: '';
+		left: calc(var(--size) * 0.55);
+		top: 0;
+		width: calc(var(--size) * 0.55);
+		height: calc(var(--size) * 0.85);
+		background: var(--color);
+		border-radius: calc(var(--size) * 0.55) calc(var(--size) * 0.55) 0 0;
+		transform: rotate(-45deg);
+		transform-origin: 0 100%;
+	}
+
+	.heart:after {
+		left: 0;
+		transform: rotate(45deg);
+		transform-origin: 100% 100%;
+	}
+</style>
